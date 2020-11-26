b32b62c8582e0b030ccf6ed8fbfe5d9dc49de326
Sami Ul Faysal <38893131+SamiUlFaysal@users.noreply.github.com>
2020-11-26 19:55:03 +0600

    Add files via upload

---
+++ vendors/main.js
@@ -0,0 +1,6 @@
+document.write(" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dicta architecto veniam.");
+
+//document.readyState(alert("Hello Masum"));
+console.log(
+    document.write(" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dicta architecto veniam.")
+);
